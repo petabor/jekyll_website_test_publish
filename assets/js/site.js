536b35b3b8fc15217801bbcbe1d04c4edb138ea7
petra borovska <petra.borovska@psychol.uni-giessen.de>
2025-10-25 15:56:26 +0200

    fix paths

--- assets/js/site.js
+++ assets/js/site.js
@@ -7,12 +7,12 @@ document.addEventListener('DOMContentLoaded', async () => {
   const mods = {};
   const load = async (name, file) => (mods[name] ??= await import(file));
 
-  await import(`${baseurl}/assets/js/behaviors/yearPanel.js`).then(m => m.init());
-  await import(`${baseurl}/assets/js/behaviors/comparePanel.js`).then(m => m.init());
-  await import(`${baseurl}/assets/js/behaviors/conflictFilter.js`).then(m => m.init());
+  await import(`/jekyll_website_test_publish/assets/js/behaviors/yearPanel.js`).then(m => m.init());
+  await import(`/jekyll_website_test_publish/assets/js/behaviors/comparePanel.js`).then(m => m.init());
+  await import(`/jekyll_website_test_publish/assets/js/behaviors/conflictFilter.js`).then(m => m.init());
 
-  if (need.has('enlarge')) await load('enlarge', `${baseurl}/assets/js/behaviors/enlarge.js`).then(m => m.init());
-  if (need.has('zoom'))    await load('zoom',    `${baseurl}/assets/js/behaviors/zoom.js`).then(m => m.init());
-  if (need.has('link'))    await load('link',    `${baseurl}/assets/js/behaviors/link.js`).then(m => m.init());
-  if (need.has('bind'))    await load('bind',    `${baseurl}/assets/js/behaviors/bind.js`).then(m => m.init());
+  if (need.has('enlarge')) await load('enlarge', `/jekyll_website_test_publish/assets/js/behaviors/enlarge.js`).then(m => m.init());
+  if (need.has('zoom'))    await load('zoom',    `/jekyll_website_test_publish/assets/js/behaviors/zoom.js`).then(m => m.init());
+  if (need.has('link'))    await load('link',    `/jekyll_website_test_publish/assets/js/behaviors/link.js`).then(m => m.init());
+  if (need.has('bind'))    await load('bind',    `/jekyll_website_test_publish/assets/js/behaviors/bind.js`).then(m => m.init());
 });
